d0b50debc9b8e07ec4c8b9edf8259ed9ecac4682
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2025-02-15 22:48:08 +0900

    confirm answer d

--- ABC/abc393/d/main.cpp
+++ ABC/abc393/d/main.cpp
@@ -7,56 +7,23 @@ int main() {
 
     cin >> n >> s;
 
-    vector<int> subscripts;
-
-    for (int i = 0; i < (int)s.size(); ++i) {
-        if (s.at(i) == '1') subscripts.push_back(i);
+    int c1 = 0;
+    for (char c : s) {
+        if (c == '1') ++c1;
     }
 
-    int pivot_sub = (int)subscripts.size() / 2;
-    int pivot = subscripts.at(pivot_sub);
-    int series_cnt = 0;
+    int now = 0;
+    int64_t ans = 0;
 
-    int now = pivot;
-    int dist = 0;
-    for (int i = pivot_sub + 1; i < (int)subscripts.size(); ++i) {
-        if (subscripts.at(i) - now == 1) {
-            series_cnt += dist;
-            ++dist;
+    for (char c : s) {
+        if (c == '0') {
+            ans += min(now, c1 - now);
         } else {
-            break;
+            ++now;
         }
-        now = subscripts.at(i);
     }
 
-    now = pivot;
-    dist = 0;
-    for (int i = pivot_sub - 1; i >= 0; --i) {
-        if (now - subscripts.at(i) == 1) {
-            series_cnt += dist;
-            ++dist;
-        } else {
-            break;
-        }
-        now = subscripts.at(i);
-    }
-
-    for (int i = 0; i < (int)subscripts.size(); ++i) {
-        subscripts.at(i) = abs(subscripts.at(i) - pivot);
-        if (subscripts.at(i) != 0) --subscripts.at(i);
-    }
-
-    int64_t count = 0;
-
-    for (int i = 0; i < (int)subscripts.size(); ++i) {
-        count += subscripts.at(i);
-    }
-
-    if ((int)subscripts.size() > 3) {
-        count -= (int)subscripts.size() - 3;
-    }
-
-    cout << count - series_cnt << endl;
+    cout << ans << endl;
 
     return 0;
 }
